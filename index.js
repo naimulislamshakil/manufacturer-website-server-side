d7d05fb101902fa7928b5f485751dde21506bc7c
naimulislamshakil <ms0413354@gmail.com>
2022-05-27 16:29:00 +0600

    deploy complete

--- index.js
+++ index.js
@@ -258,7 +258,7 @@ async function run() {
         res.send(result);
       }
     );
-
+    // add admin
     // get specifie order details
     app.get("/payment/:id", jwtVerify, async (req, res) => {
       const id = req.params.id;
